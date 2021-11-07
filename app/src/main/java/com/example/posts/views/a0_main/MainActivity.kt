f1ec3167d735975ad20871ba0f825d8bae15ce15
Stanley Eng <stanley.eng.gt@gmail.com>
2021-11-07 15:36:33 +1300

    Commit- Added all dependencies, created navigation component and fragments

--- app/src/main/java/com/example/posts/views/a0_main/MainActivity.kt
+++ app/src/main/java/com/example/posts/views/a0_main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.posts
+package com.example.posts.views.a0_main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.posts.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
